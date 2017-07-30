eb27cf308fbde43f6bdc9502bfd265050074fa35
Mila <llysenko23@rambler.ru>
2017-07-30 17:25:01 +0300

    *config

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-//var cool = require('cool-ascii-faces');
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-var router = express.Router();
-var routes = require('./index');
-
-/* ???????? ??????? ????????. */
-router.get('/', function(req, res) {
-  res.render('index');
-});
-
-module.exports = router;
-
-app.use('/', routes);
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/src'));
-//app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('src', __dirname + '/src');
-//app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-/*app.get('/', function(request, response) {
-  response.render('/index')
-});*/
-/*
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
 app.get('/cool', function(request, response) {
   response.send(cool());
-});*/
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
