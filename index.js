41af7e3d4f83900eb9cf81c9b4d64aba0c5108a1
Mila <llysenko23@rambler.ru>
2017-08-01 12:45:36 +0300

    *

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 // создаём Express-приложение
 var app = express();
 // сообщаем Node где лежат ресурсы сайта
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/build'));
 // создаём маршрут для главной страницы
 // http://localhost:8080/
 app.get('/', function(req, res) {
@@ -12,6 +12,6 @@ app.get('/', function(req, res) {
 });
 
 // запускаем сервер на порту 8080
-app.listen(8080);
-// отправляем сообщение
-console.log('Сервер стартовал!');
+app.listen( port, function () {
+  console.log( 'Our app is running on http://localhost:' + port );
+} );
