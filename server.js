2c2906d558ab1a9b33db62a5ce386688e5dd76d6
Mila <llysenko23@rambler.ru>
2017-08-21 12:35:56 +0300

    * fixed typo in styles; + connection to db; + dump-file for migrate of local db to heroku;

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var express = require( 'express' );
 var app = express();
-
+var bodyParser = require( 'body-parser' );
+//var pg = require('pg');
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
+var pgp = require( "pg-promise" )( /*options*/ );
+var db = pgp( "postgres://postgres:root@localhost/resume" ); // "postgres://username:password@host:port/database"
+
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+
+//CORS middleware
+app.use( function ( req, res, next ) {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+    next();
+} );
+app.get( '/portfolio', function ( req, res ) {
+    
+    db.any( "SELECT * FROM portfolio" )
+        .then( function ( data ) {
+            res.send(data);
+            //console.log( "DATA:", data );
+        } )
+        .catch( function ( error ) {
+            console.log( "ERROR:", error );
+        } );
+} );
+
 
 // set the view engine to ejs
 //app.set( 'view engine', 'ejs' );
@@ -15,7 +40,7 @@ app.use( express.static( __dirname + '/build' ) );
 app.get( '/', function ( req, res ) {
     
     // ejs render automatically looks in the views folder
-    res.sendFile(path.join(__dirname, '/build', 'index.html'));
+    res.sendFile( path.join( __dirname, '/build', 'index.html' ) );
 } );
 
 app.listen( port, function () {
